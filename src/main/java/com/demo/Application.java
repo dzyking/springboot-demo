a29a375ebc3dba938ca8c495a8794d7805765fcb
dzy <469246557@qq.com>
2021-07-29 14:47:27 +0800

    SchedulerLock分布式定时任务锁

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -1,11 +1,13 @@
 package com.demo;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 @EnableScheduling
 @SpringBootApplication
 @MapperScan(basePackages = "com.demo.mapper")
--- src/main/java/com/demo/scheduled/ScheduledTest.java
+++ src/main/java/com/demo/scheduled/ScheduledTest.java
@@ -1,6 +1,7 @@
 package com.demo.scheduled;
 
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,8 @@ public class ScheduledTest {
 
     //每分钟执行一次
     @Scheduled(cron = "0 0/1 * * * ? ")
+    //每次锁定一分钟(多服务部署)
+    @SchedulerLock(name = "test", lockAtMostFor = 60000, lockAtLeastFor = 60000)
     public void test() {
         log.info("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
